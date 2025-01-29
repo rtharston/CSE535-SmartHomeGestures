41a7d81a3a387ff230fd4a4685cd77b7ea283e14
Reed Harston <rtharston@gmail.com>
2025-01-29 07:35:39 -0700

    Add basic Spinner element to gesture select fragment to select the gesture

--- app/src/main/java/com/example/smarthomegestures/GestureSelectFragment.java
+++ app/src/main/java/com/example/smarthomegestures/GestureSelectFragment.java
@@ -1,9 +1,12 @@
 package com.example.smarthomegestures;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,9 +14,13 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.smarthomegestures.databinding.FragmentGestureSelectBinding;
 
-public class GestureSelectFragment extends Fragment {
+import java.util.Optional;
+
+public class GestureSelectFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     private FragmentGestureSelectBinding binding;
+
+    private Optional<String> selection;
 
     @Override
     public View onCreateView(
@@ -22,6 +29,19 @@ public class GestureSelectFragment extends Fragment {
     ) {
 
         binding = FragmentGestureSelectBinding.inflate(inflater, container, false);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                getActivity().getBaseContext(),
+                R.array.gestures_array,
+                android.R.layout.simple_spinner_item
+        );
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        binding.gestureSelectSpinner.setAdapter(adapter);
+        binding.gestureSelectSpinner.setOnItemSelectedListener(this);
+
         return binding.getRoot();
 
     }
@@ -39,6 +59,18 @@ public class GestureSelectFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Object thing = parent.getItemAtPosition(position);
+        Log.d("gestureSelect", thing.toString());
+        selection = Optional.of(thing.toString());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        selection = Optional.empty();
     }
 
 }
