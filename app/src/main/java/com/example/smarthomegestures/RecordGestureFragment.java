0b47edde4e6105d155bb9ed8703d72a827e18387
Reed Harston <rtharston@gmail.com>
2025-02-01 22:46:54 -0700

    Prompt user for permissions to access camera

--- app/src/main/java/com/example/smarthomegestures/RecordGestureFragment.java
+++ app/src/main/java/com/example/smarthomegestures/RecordGestureFragment.java
@@ -1,12 +1,18 @@
 package com.example.smarthomegestures;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.Manifest;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.smarthomegestures.databinding.FragmentRecordGestureBinding;
@@ -14,6 +20,17 @@ import com.example.smarthomegestures.databinding.FragmentRecordGestureBinding;
 public class RecordGestureFragment extends Fragment {
 
     private FragmentRecordGestureBinding binding;
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    startCamera();
+                } else {
+                    Toast.makeText(getContext(),
+                            "Permissions denied. You can't record without camera permissions.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
 
     @Override
     public View onCreateView(
@@ -29,10 +46,27 @@ public class RecordGestureFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-         binding.videoCaptureButton.setOnClickListener(v ->
-             Log.d("buttonRecordGesture", "Record gesture")
-            // TODO: save recording
-         );
+        binding.videoCaptureButton.setOnClickListener(v ->
+                        Log.d("buttonRecordGesture", "Record gesture")
+                // TODO: save recording
+        );
+
+        if (checkPermissions()) {
+            startCamera();
+        } else {
+            requestPermissions();
+        }
+    }
+
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+    }
+
+    private void startCamera() {
     }
 
     @Override
